acc8cebe229c0ef3cfd410edd299ce88e183d493
Nkl-4 <nklnikil4@gmail.com>
2020-03-29 16:27:12 +0530

    csv , plot working

--- datascrap.py
+++ datascrap.py
@@ -62,7 +62,6 @@ pdata = {
 }
 df = pd.DataFrame(pdata,columns=['Name of State / UT','Total Confirmed cases (Indian National)','Total Confirmed cases ( Foreign National )','Cured/Discharged/Migrated', 'Death'])
 
-#df.to_csv('temp.csv', encoding='utf-8', index=False)
+df.to_csv('temp.csv', encoding='utf-8', index=False)
 df.plot(kind='barh',x='Name of State / UT', y='Total Confirmed cases (Indian National)',figsize=(18,8))
-# plt.bar(sname, Tcin)
 plt.savefig('plot.png')
