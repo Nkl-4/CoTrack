281188918483610a91f5575a3644092d1360a608
Nkl-4 <nklnikil4@gmail.com>
2020-03-29 16:04:46 +0530

    plot done

--- datascrap.py
+++ datascrap.py
@@ -13,6 +13,12 @@ soup = BeautifulSoup(html_content, "lxml")
 
 data = []
 headers = []
+sname = []
+Tcin = []
+Tcfn = []
+cured = []
+death = []
+
 table_div = soup.find('div', {'id':'cases'})
 table = table_div.find('table')
 
@@ -23,15 +29,40 @@ for h in head:
 table_body = table.find("tbody")
 rows  = table_body.find_all('tr')
 
-data.append(headers)
+
+
+#data.append(headers)
 
 for row in rows:
     cols = row.find_all('td')
     cols = [ele.text.strip() for ele in cols]
     data.append([ele for ele in cols if ele])
 
-data[-1].insert(0,28)
-df = pd.DataFrame(data)
+#data[-1].insert(0,None)
+total = data[-1]
+data = data[:-1]
+#print(headers)
+
+
+
+for d in data:
+    sname.append(d[1])
+    Tcin.append(int(d[2]))
+    Tcfn.append(int(d[3]))
+    cured.append(int(d[4]))
+    death.append(int(d[5]))
+
+
+pdata = { 
+    'Name of State / UT':sname, 
+    'Total Confirmed cases (Indian National)':Tcin, 
+    'Total Confirmed cases ( Foreign National )':Tcfn, 
+    'Cured/Discharged/Migrated':cured, 
+    'Death':death
+}
+df = pd.DataFrame(pdata,columns=['Name of State / UT','Total Confirmed cases (Indian National)','Total Confirmed cases ( Foreign National )','Cured/Discharged/Migrated', 'Death'])
 
 #df.to_csv('temp.csv', encoding='utf-8', index=False)
-print(df.iloc[1])
+df.plot(kind='barh',x='Name of State / UT', y='Total Confirmed cases (Indian National)',figsize=(18,8))
+# plt.bar(sname, Tcin)
+plt.savefig('plot.png')
